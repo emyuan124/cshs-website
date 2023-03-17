34a5646c22dda5b72e8152231fdcaa62767d524f
SirAlienTheGreat <allen.macfarland@gmail.com>
2023-03-17 14:28:12 -0600

    Add march meeting slides

--- slides.php
+++ slides.php
@@ -8,7 +8,19 @@
   <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
   <div class="pt-[60px] flex flex-col justify-between min-h-screen">
     <div class="px-10 flex flex-wrap justify-center gap-10 pb-10">
+
+    
       
+      <div class="group card card-compact w-[450px] xl:w-1/3 bg-base-200 shadow-xl mt-10 cursor-pointer hover:bg-primary transition duration-300">
+        <a target="_blank" href="https://docs.google.com/presentation/d/1wjiDybCBNOZYH4vJT8jkK7qPPKGvLNTrBI9AMgAdSIY/edit?usp=sharing">
+        <figure><img src="img/meeting6.avif" onerror="this.src='img/meeting6.png'" alt="Shoes" class="group-hover:opacity-75 transition duration-300" /></figure>
+        <div class="card-body">
+          <div class="badge badge-secondary mx-auto">Monthly Meeting</div>
+          <h2 class="card-title text-2xl flex justify-center group-hover:text-white transition duration-300">Mar 8, 2023</h2>
+        </div>
+        </a>
+      </div>
+
       <div class="group card card-compact w-[450px] xl:w-1/3 bg-base-200 shadow-xl mt-10 cursor-pointer hover:bg-primary transition duration-300">
         <a target="_blank" href="https://docs.google.com/presentation/d/1U33IyWbRv4CzWKhfr5xHUBfddTN9yPi3Mi46KZPCQXg/edit?usp=sharing">
         <figure><img src="img/meeting5.avif" onerror="this.src='img/meeting5.png'" alt="Shoes" class="group-hover:opacity-75 transition duration-300" /></figure>
