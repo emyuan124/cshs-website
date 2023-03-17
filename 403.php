ef5e6dc5ec8038d17f9c19f3ca007d5b7a50d896
SirAlienTheGreat <allen.macfarland@gmail.com>
2023-03-17 11:00:50 -0600

    add custom 403 page

---
+++ 403.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FRHS Tech</title>
+    <?php include $_SERVER["DOCUMENT_ROOT"].'/head.php'; ?>
+</head>
+<body data-theme="night">
+  <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
+  <div class="pt-[60px] flex flex-col justify-between min-h-screen">
+    <div class="text-center mt-12">
+        <h1 class="text-5xl font-bold text-secondary">Error 403 - Forbidden</h1>
+        You may have been trying to access a directory. If you're an end user, this shouldn't happen.
+    </div>
+  </div>
+  <?php include $_SERVER["DOCUMENT_ROOT"].'/footer.php'; ?>
+</body>
+</html>
