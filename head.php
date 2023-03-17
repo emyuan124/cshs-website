93836cd710e512883a5f63192efe5838572a4081
SirAlienTheGreat <allen.macfarland@gmail.com>
2023-03-17 10:21:24 -0600

    Absolute paths, add MAWW

--- head.php
+++ head.php
@@ -3,6 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/daisyui@2.42.1/dist/full.css" rel="stylesheet" type="text/css" />
 <script src="https://cdn.tailwindcss.com"></script>
-<script src="js/exports.js"></script>
-<script src="js/nav.js"></script>
-<script src="js/session.js"></script>
+<script src="/js/exports.js"></script>
+<script src="/js/nav.js"></script>
+<script src="/js/session.js"></script>
