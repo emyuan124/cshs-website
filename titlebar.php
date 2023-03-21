008eb14287622263d468be4c082b97f620de221a
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-03-20 21:06:13 -0600

    nav fix for maww

--- titlebar.php
+++ titlebar.php
@@ -54,6 +54,17 @@
             MAWW
             <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
           </a>
+          <ul class="p-2 bg-base-100 z-10">
+            <li><a href="/maww/Avery_Page">Meet Avery</a></li>
+            <li><a href="/maww/foodtrucks">Food Trucks</a></li>
+            <li><a href="/maww/saturday">Saturday's Event</a></li>
+            <li><a href="/maww/sunday">Sunday's Event</a></li>
+            <li><a href="/maww/Monday_Event">Monday's Event</a></li>
+            <li><a href="/maww/Tuesday_Event">Tuesday's Event</a></li>
+            <li><a href="/maww/wednesday">Wednesday's Event</a></li>
+            <li><a href="/maww/thursday">Thursday's Event</a></li>
+            <li><a href="/maww/friday">Friday's Event</a></li>
+          </ul>
         </li>
         <label class="swap swap-rotate px-3">
           <input id="theme_switch" type="checkbox" onchange="changeTheme(this)"/>
