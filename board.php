d10f1cd5393c20c1195d04905e7b03e5cfd44413
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-09-09 16:34:57 -0600

    changed board members

--- board.php
+++ board.php
@@ -5,21 +5,7 @@
     <?php include $_SERVER["DOCUMENT_ROOT"].'/head.php'; ?>
 </head>
 <body data-theme="night">
-  <div class="hidden text-center  fixed flex justify-center items-center z-20 top-0 left-0 w-3/4 h-screen bg-base-300" id="sidebar">
-    <div class="text-xl font-semibold">
-      <a href="beardify"><div class="p-5 border-b border-base-content">Beardify</div></a>
-      <a href="cshs"><div class="p-5 border-b border-base-content">CSHS</div></a>
-      <a href="codingclub"><div class="p-5 border-b border-base-content">Coding Club</div></a>
-      <a href="404"><div class="p-5 border-b border-base-content" >CyberCats</div></a>
-      <div class="p-5">
-        <label class="swap swap-rotate px-3">
-          <input id="theme_switch" type="checkbox" onchange="changeTheme(this)"/>
-            <svg class="swap-on fill-current w-7 h-7" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-            <svg class="swap-off fill-current w-7 h-7" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
-        </label>
-      </div>
-    </div>
-  </div>
+  <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
   <div class="fixed z-10 navbar bg-base-200 shadow-xl" id="nav">
     <div class="flex-1">
       <a class="btn btn-ghost normal-case text-2xl" href="index">
@@ -56,37 +42,7 @@
   </div>
   <div class="pt-[60px] px-10 flex flex-wrap justify-center gap-10 pb-10">
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/kerem.avif" onerror="this.src='img/kerem.jpg'" alt="Kerem" class="rounded-xl"/></figure>
-      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
-        <div>
-          <h2 class="card-title justify-center text-2xl">Kerem Gurkan</h2>
-          <div class="badge badge-secondary my-2">President</div>
-          <p>Kerem's a student by day, a film producer by afternoon, a web designer and Cybersecurity analyst by night, and asleep by morning.</p>
-        </div>
-      </div>
-    </div>
-    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/brandon.avif" onerror="this.src='img/brandon.jpg'" alt="Album" class="rounded-xl"/></figure>
-      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
-        <div>
-          <h2 class="card-title justify-center text-2xl">Brandon Winecki</h2>
-          <div class="badge badge-secondary my-2">Vice-President</div>
-          <p>Brandon is an avid twisty puzzle collector and speedcuber. He can solve the Rubik's cube in 15 seconds, and hopes to participate in local competitions as COVID winds down.</p>
-        </div>
-      </div>
-    </div>
-    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/caroline.avif" onerror="this.src='img/caroline.jpg'" alt="Album" class="rounded-xl"/></figure>
-      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
-        <div>
-          <h2 class="card-title justify-center text-2xl">Caroline Menten</h2>
-          <div class="badge badge-secondary my-2">Vice-President</div>
-          <p>Caroline has been to 10 concerts in the past year, and plans to continue going to as many concerts as she can!</p>
-        </div>
-      </div>
-    </div>
-    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/allen.avif" onerror="this.src='img/allen.jpg'" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/allen.jpg" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
           <h2 class="card-title justify-center text-2xl">Allen Macfarland</h2>
@@ -96,7 +52,27 @@
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/connor.avif" onerror="this.src='img/connor.jpg'" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/sujal.jpg" alt="Album" class="rounded-xl"/></figure>
+      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
+        <div>
+          <h2 class="card-title justify-center text-2xl">Sujal Dhakal</h2>
+          <div class="badge badge-secondary my-2">Vice-President</div>
+          <p>-</p>
+        </div>
+      </div>
+    </div>
+    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/ryan.jpg" alt="Album" class="rounded-xl"/></figure>
+      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
+        <div>
+          <h2 class="card-title justify-center text-2xl">Ryan Tally</h2>
+          <div class="badge badge-secondary my-2">Vice-President</div>
+          <p>-</p>
+        </div>
+      </div>
+    </div>
+    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/connor.avif" onerror="this.src='img/connor.jpg'" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
           <h2 class="card-title justify-center text-2xl">Connor Dietrich</h2>
@@ -106,12 +82,22 @@
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/danny.avif" onerror="this.src='img/danny.jpg'" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/harsha.jpg" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
-          <h2 class="card-title justify-center text-2xl">Danny Jeong</h2>
-          <div class="badge badge-secondary my-2">Cybercats Liaison</div>
-          <p>Danny is trilingual and loves listening to music</p>
+          <h2 class="card-title justify-center text-2xl">Shree Harsha Kalavagunta</h2>
+          <div class="badge badge-secondary my-2">Secretary</div>
+          <p>-</p>
+        </div>
+      </div>
+    </div>
+    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/emmy.jpg" alt="Album" class="rounded-xl"/></figure>
+      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
+        <div>
+          <h2 class="card-title justify-center text-2xl">Emmy Yuan</h2>
+          <div class="badge badge-secondary my-2">Treasurer</div>
+          <p>-</p>
         </div>
       </div>
     </div>
--- testing.php
+++
@@ -1,2 +0,0 @@
-<!DOCTYPE html>
-<meta http-equiv="Refresh" content="0; url='https://google.com'" />
