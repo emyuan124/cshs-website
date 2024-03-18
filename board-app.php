518d29e99a734e4e94332d49a513694c9079dbcb
SirAlienTheGreat <79958059+SirAlienTheGreat@users.noreply.github.com>
2024-03-18 13:30:00 -0600

    Update board-app for 2024 applications

--- board-app.php
+++ board-app.php
@@ -1,2 +1,2 @@
 <!DOCTYPE html>
-<meta http-equiv="Refresh" content="0; url='https://docs.google.com/forms/d/e/1FAIpQLSfgmhpJWKyfllG8-sjwW1gLsG0NGlY73tpFfHjbxMo1MJlXSQ/viewform?usp=share_link'" />
+<meta http-equiv="Refresh" content="0; url='https://docs.google.com/forms/d/e/1FAIpQLScE2OZN9NHb6kfFyqOV8VrNHdL1LH-KYVhrIQSF1QpmaliPSA/viewform?usp=sf_link'" />
