142acad4cd67dbc8a95886e140d848e8c97e005d
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-08-29 17:43:03 -0600

    Update board.php
    Started updating the board

--- board.php
+++ board.php
@@ -8,12 +8,12 @@
   <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
   <div class="pt-[60px] px-10 flex flex-wrap justify-center gap-10 pb-10">
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/allen.jpg" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/emmy.jpg" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
-          <h2 class="card-title justify-center text-2xl">Allen Macfarland</h2>
-          <div class="badge badge-secondary my-2">Developer</div>
-          <p>Allen advocates for rust, the future of programming, every day because he is not afraid of the future, but instead welcomes it with open arms</p>
+          <h2 class="card-title justify-center text-2xl">Emmy Yuan</h2>
+          <div class="badge badge-secondary my-2">President</div>
+          <p></p>
         </div>
       </div>
     </div>
