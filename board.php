43a8f6f0f26bb628e4dc65377541cde551b63269
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-08-29 18:08:17 -0600

    Update board
    Moved around names and titles, and used thing3.png as a filler for people who don't have pictures yet

--- board.php
+++ board.php
@@ -13,56 +13,56 @@
         <div>
           <h2 class="card-title justify-center text-2xl">Emmy Yuan</h2>
           <div class="badge badge-secondary my-2">President</div>
-          <p></p>
-        </div>
-      </div>
-    </div>
-    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/connor.avif" alt="Album" class="rounded-xl"/></figure>
-      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
-        <div>
-          <h2 class="card-title justify-center text-2xl">Connor Dietrich</h2>
-          <div class="badge badge-secondary my-2">Vice-President</div>
           <p>-</p>
         </div>
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/ryan.jpg" alt="Album" class="rounded-xl"/></figure>
-      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
-        <div>
-          <h2 class="card-title justify-center text-2xl">Ryan Tally</h2>
-          <div class="badge badge-secondary my-2">Vice-President</div>
-          <p>-</p>
-        </div>
-      </div>
-    </div>
-    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/connor.avif" onerror="this.src='img/connor.jpg'" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/connor.jpg" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
           <h2 class="card-title justify-center text-2xl">Connor Dietrich</h2>
-          <div class="badge badge-secondary my-2">Secretary</div>
+          <div class="badge badge-secondary my-2">Vice-President</div>
           <p>I started programming in Java when I was 8½</p>
         </div>
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/harsha.jpg" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/thing3.png" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
-          <h2 class="card-title justify-center text-2xl">Shree Harsha Kalavagunta</h2>
+          <h2 class="card-title justify-center text-2xl">Elijah Stow</h2>
+          <div class="badge badge-secondary my-2">Developer</div>
+          <p>-</p>
+        </div>
+      </div>
+    </div>
+    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/thing3.png" onerror="this.src='img/connor.jpg'" alt="Album" class="rounded-xl"/></figure>
+      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
+        <div>
+          <h2 class="card-title justify-center text-2xl">Cooper Sipperly</h2>
+          <div class="badge badge-secondary my-2">Coordinator</div>
+          <p>-</p>
+        </div>
+      </div>
+    </div>
+    <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/thing3.png" alt="Album" class="rounded-xl"/></figure>
+      <div class="card-body flex justify-center items-center text-center lg:w-1/2">
+        <div>
+          <h2 class="card-title justify-center text-2xl">Athena Yang</h2>
           <div class="badge badge-secondary my-2">Secretary</div>
           <p>-</p>
         </div>
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/emmy.jpg" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/thing3.png" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
-          <h2 class="card-title justify-center text-2xl">Emmy Yuan</h2>
-          <div class="badge badge-secondary my-2">Treasurer</div>
+          <h2 class="card-title justify-center text-2xl">Sachit Srinivas</h2>
+          <div class="badge badge-secondary my-2">Communications</div>
           <p>-</p>
         </div>
       </div>
