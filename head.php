6f2b1bb1140d936eddcde1cc4f392f346c67a040
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-08-29 20:10:10 -0600

    Added favicon

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css" rel="stylesheet" type="text/css" />
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+<link rel="shortcut icon" href="img/logo.ico" type="image/x-icon"/>
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="/js/exports.js"></script>
 <script src="/js/nav.js"></script>
