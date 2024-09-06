76445359787a504eba90bf88fd3b1530e9f910cb
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-06 15:54:48 -0600

    Update head.php
    Trying to get the favicon to work

--- head.php
+++ head.php
@@ -1,9 +1,9 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css" rel="stylesheet" type="text/css" />
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
-<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="/js/exports.js"></script>
 <script src="/js/nav.js"></script>
