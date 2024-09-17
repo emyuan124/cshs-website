91952e6a2d9cf2a192e7848f749d3875f3b33b63
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-17 10:48:54 -0600

    Update index.php
    fixed an image reference

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <div class="carousel w-full pt-[60px]" id="carousel">
     <div id="slide0" class="carousel-item relative w-full">
       <a href="https://forms.gle/S9Zj1aTc13AQUdGr7">
-        <img src="img/Coding Club 2024-25 Banner.png" class="w-full" />
+        <img src="img/Coding Club-2024-25-Banner.png" class="w-full" />
       </a>
       <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
         <a onclick="scrollCarousel('#slide3')" class="btn btn-circle max-md:hidden">❮</a>
