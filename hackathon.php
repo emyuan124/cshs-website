453827ca5d18e4e39a0506a9bcadefced6f9bfd2
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-17 11:09:12 -0600

    Changed link

--- hackathon.php
+++ hackathon.php
@@ -1,2 +1,2 @@
 <!DOCTYPE html>
-<meta http-equiv="Refresh" content="0; url='https://docs.google.com/forms/d/e/1FAIpQLSd0yI5hCOB8czPVdgF0Ct7YnEDfR3_XZw9eO3owlaZzCqjyxQ/viewform?usp=sf_link'" />
+<meta http-equiv="Refresh" content="0; url='https://forms.gle/zq5XLQuAekRawqYN8'" />
