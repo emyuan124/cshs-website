8e3251d6a112176031588c0b4e9e6a0c1a6b0e82
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 13:54:47 -0600

    Update hackathon.php

--- hackathon.php
+++ hackathon.php
@@ -13,7 +13,7 @@
     <div class="hero-content flex-col lg:flex-row-reverse gap-6 lg:gap-24">
       <img src="img/hero2.avif" onerror="this.src='img/hero2.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
       <div>
-        <h1 class="text-5xl font-bold">Why Should I Join?</h1>
+        <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
         <p class="py-6">5C's is dedicated to making promoting Computer Science in Fossil Ridge. By joining one of the 5C's club, you'll be able to recieve early and priority access to our various events while furthering your knowledge in Computer Science. You'll also have the oppurtunity to work on our in-house service projects and internships, making our organizations a great addition to for college applications. If that sounds a bit difficult, worry not! We provide our own workshops to teach you real Computer Science skills, and we will be providing numerous guest speakers to come in and give you an idea on what it is like to do Computer Science as a career.</p>
       </div>
     </div>
