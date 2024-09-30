9c513ba919983ef1a87e92fed730cace559f3524
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 10:51:22 -0600

    Update hackathon.php

--- hackathon.php
+++ hackathon.php
@@ -9,7 +9,7 @@
   <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
 
     <div class="logo py-12 lg:py-32 bg-base-100">
-      <div class="logo-content flex-col lg:flex-row gap-6 lg:gap-24">
+      <div class="logo-content flex-col lg:flex-row-reverse gap-6 lg:gap-24">
         <img src="img/hero.avif" onerror="this.src='img/logo.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
