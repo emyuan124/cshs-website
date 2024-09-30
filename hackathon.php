c8901ca72abfeb46b95c0a91a5ecc28ec30efb72
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 10:44:40 -0600

    Update hackathon.php

--- hackathon.php
+++ hackathon.php
@@ -7,8 +7,16 @@
 </head>
 <body class="text-center lg:text-left">
   <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
-    <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
-    <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent!</p>
+
+    <div class="logo py-12 lg:py-32 bg-base-100">
+      <div class="logo-content flex-col lg:flex-row gap-6 lg:gap-24">
+        <img src="img/hero.avif" onerror="this.src='img/logo.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
+          <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent!</p>
+        </div>
+      </div>
+  </div>
     
   
   <?php include $_SERVER["DOCUMENT_ROOT"].'/footer.php'; ?>
