9aa07c2aeb11652bb15292eabe4bdceeeeb9d5e3
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 10:32:52 -0600

    Update hackathon.php
    Working on making a dedicated hackathon page

--- hackathon.php
+++ hackathon.php
@@ -1,2 +1,16 @@
 <!DOCTYPE html>
-<meta http-equiv="Refresh" content="0; url='https://forms.gle/zq5XLQuAekRawqYN8'" />
+<html lang="en">
+<head>
+    <title>FRHS Tech</title>
+    <script src="js/carousel.js"></script>
+    <?php include $_SERVER["DOCUMENT_ROOT"].'/head.php'; ?>
+</head>
+<body class="text-center lg:text-left">
+  <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
+    <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
+    <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent!</p>
+    
+  
+  <?php include $_SERVER["DOCUMENT_ROOT"].'/footer.php'; ?>
+</body>
+</html>
