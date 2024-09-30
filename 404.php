1c630e8deac55970995c850aea040b27e388a4bb
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 14:08:36 -0600

    Update 404.php

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
   <?php include $_SERVER["DOCUMENT_ROOT"].'/titlebar.php'; ?>
   <div class="pt-[60px] flex flex-col justify-between min-h-screen">
     <div class="text-center mt-12">
-        This page is currently still in construction. Please come back later!
+        Something went wrong. Please come back later!
     </div>
   </div>
   <?php include $_SERVER["DOCUMENT_ROOT"].'/footer.php'; ?>
