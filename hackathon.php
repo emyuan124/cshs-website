8a238347f15539cbd0d0b3d3435f7025a5dddb5f
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 14:04:09 -0600

    Update hackathon.php

--- hackathon.php
+++ hackathon.php
@@ -14,7 +14,7 @@
       <img src="img/logo.png" onerror="this.src='img/hero2.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
       <div>
         <h1 class="text-5xl font-bold">What is a Hackathon?</h1>
-        <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent!</p>
+        <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent! Big ideas like Twitter and GroupME have come out of big HackAThons.</p>
       </div>
     </div>
   </div>
