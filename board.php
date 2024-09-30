23d3591840dd7ab76baec62ee7dafaca9a04450e
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 14:16:09 -0600

    Update board.php

--- board.php
+++ board.php
@@ -28,7 +28,7 @@
       </div>
     </div>
     <div class="card lg:card-side bg-base-200 shadow-xl p-2 w-[600px] 2xl:w-2/5 mt-10">
-      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/thing3.png" alt="Album" class="rounded-xl"/></figure>
+      <figure class="h-[300px] md:w-1/2 lg:w-1/2 mx-auto md:mt-10 lg:mt-0" ><img src="img/board/24-25/elijah.jpeg" alt="Album" class="rounded-xl"/></figure>
       <div class="card-body flex justify-center items-center text-center lg:w-1/2">
         <div>
           <h2 class="card-title justify-center text-2xl">Elijah Stow</h2>
