0bc2ce4459a3f14d1c780005b2ca09749c9161ee
ManyATrueFail <150209270+ManyATrueFail@users.noreply.github.com>
2024-09-30 13:51:02 -0600

    Update hackathon.php

--- hackathon.php
+++ hackathon.php
@@ -10,7 +10,7 @@
 
 <div class="logo py-12 lg:py-32 bg-base-100">
     <div class="logo-content flex-col lg:flex-row-reverse gap-6 lg:gap-24">
-      <img src="img/hero2.avif" onerror="this.src='img/logo.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
+      <img src="img/logo.png" onerror="this.src='img/logo.png'" class="max-md:w-[90%] max-w-sm rounded-lg shadow-2xl" />
       <div>
         <h1 class="text-5xl font-bold">What is?</h1>
         <p class="py-6">A Hackathon is a competition where you create an app to a theme in a short duration of time. Tons of big companies have hackathons yearly to find new talent!</p>
